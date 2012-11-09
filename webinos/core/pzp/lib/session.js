cc893f5f9d86faee1943077e295f7b2829e4e96a
Toby Ealden <toby.ealden@gmail.com>
2012-11-09 10:17:04 +0000

    Manual merged of pull request 288
    Restructure webinos folder hierarchy.
    
    Jira issue: wp-508

---
+++ webinos/core/pzp/lib/session.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "common"            : require("./session_messageProcessing"),
+  "configuration"     : require("./session_configuration"),
+  "schema"            : require("./session_schema"),
+};
